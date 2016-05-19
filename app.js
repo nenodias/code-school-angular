41edb7d77dfe376a2eeb19d4946df9595fab0cd7
Horácio Dias <horacio.baptista@supportweb.com>
2016-05-19 08:29:39 -0300

    Lesson 2

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('store', []);
+(function(){
+	
+	var app = angular.module('store', []);
+	var gem = {
+		name : "Dodecahedron",
+		price:2.96,
+		description: "I don't know what is it"
+	};
+	
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+})();
