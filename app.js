b9c45d60f60e88a97d12bcb1b5e519523bc80c5e
Horácio Dias <horacio.dias92@gmail.com>
2016-05-21 00:44:59 -0300

    Lesson 5 - refactoring the directiver for another module

--- app.js
+++ app.js
@@ -1,67 +1,15 @@
 (function() {
 
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-directives']);
     app.controller('StoreController', function() {
         this.products = gems;
     });
-
-    app.directive('productGallery', function(){
-        return{
-            restrict:'E',
-            templateUrl:'product-gallery.html',
-            controller: function(){
-                this.current = 0;
-                this.setCurrent = function(imageNumber){
-                    this.current = imageNumber || 0;
-                };
-            },
-            controllerAs:'gallery'
-        };
-    });
+    
     app.controller("ReviewController", function() {
         this.review = {};
         this.addReview = function(product) {
             product.reviews.push(this.review);
             this.review = {};
-        };
-    });
-
-    app.directive('productTabs', function(){
-        return {
-            restrict:'E',
-            templateUrl:'product-tabs.html',
-            controller: function() {
-                this.tab = 1;
-
-                this.isSet = function(checkTab) {
-                    return this.tab === checkTab;
-                };
-                this.setTab = function(setTab) {
-                    this.tab = setTab;
-                };
-            },
-            controllerAs:'tab'
-        };
-    });
-
-    app.directive('productDescription',function(){
-        return {
-            restrict:'E',
-            templateUrl:'product-description.html'
-        };
-    });
-
-    app.directive('productSpecs',function(){
-        return {
-            restrict:'A',
-            templateUrl:'product-specs.html'
-        };
-    });
-
-    app.directive('productReviews',function(){
-        return {
-            restrict:'E',
-            templateUrl:'product-reviews.html'
         };
     });
 
